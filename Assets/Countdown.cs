f5789d8562b07b653d5c2573c13b82d0280e645c
JASON-SASTRA-LA\jason sastra <jason121301@gmail.com>
2021-02-20 22:29:35 -0500

    Finished Hand Game, go play

---
+++ Assets/Countdown.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
+    public Text timeText;
+    public AudioSource m_MyAudioSource;
+
+    private void Start()
+    {
+        // Starts the timer automatically
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                Debug.Log("You Have Won!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+                timeText.text = string.Format("You Have Woken Up!!");
+                Time.timeScale = 0;
+                m_MyAudioSource.Stop();
+            }
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
