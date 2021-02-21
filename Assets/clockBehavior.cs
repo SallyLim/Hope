8a33c3ae6df8dab2a85092c4f1bf7da6416a97a1
JASON-SASTRA-LA\jason sastra <jason121301@gmail.com>
2021-02-20 20:41:52 -0500

    Added table into hand game, hand now moves to the direction of the clock, added more randomness to it

---
+++ Assets/clockBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clockBehavior : MonoBehaviour
+{   
+    public AudioSource m_MyAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        m_MyAudioSource.Stop();
+    }
+}
