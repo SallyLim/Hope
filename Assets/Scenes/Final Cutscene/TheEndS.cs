12ce9330e10a2968d65b009b76bc00029dc0dee7
VincentSastra <vincent.sastra@gmail.com>
2021-02-21 04:22:47 -0800

    FFFFFF IIIIIIII NNNNNN AAAAAA LLLLLL

---
+++ Assets/Scenes/Final Cutscene/TheEndS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheEndS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDisable() {
+        SceneManager.LoadScene("End Roll");
+    }
+}
