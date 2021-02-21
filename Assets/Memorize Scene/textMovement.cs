6544aed5682a57f242811e3e6d16322fa4eac38c
Brennan <thetrunobody@gmail.com>
2021-02-21 15:42:46 +0700

    changed moving to fading in

--- Assets/Memorize Scene/textMovement.cs
+++ Assets/Memorize Scene/textMovement.cs
@@ -14,18 +14,26 @@ public class textMovement : MonoBehaviour
 
     private float minSpeed = 1f;
     private float maxSpeed = 5f;
+    private float frame = 0;
+    
 
     void FixedUpdate()
     {
-    textComponent1.transform.position = Vector3.MoveTowards (textComponent1.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+    //textComponent1.transform.position = Vector3.MoveTowards (textComponent1.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
 
-    textComponent2.transform.position = Vector3.MoveTowards (textComponent2.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+    //textComponent2.transform.position = Vector3.MoveTowards (textComponent2.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
 
-    textComponent3.transform.position = Vector3.MoveTowards (textComponent3.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+    //textComponent3.transform.position = Vector3.MoveTowards (textComponent3.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
 
-    textComponent4.transform.position = Vector3.MoveTowards (textComponent4.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+    //textComponent4.transform.position = Vector3.MoveTowards (textComponent4.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
 
-    if (textComponent1.transform.position == targetText1.transform.position)
+    //if (textComponent1.transform.position == targetText1.transform.position)
+
+    if (frame <= 420)
+    {
+        frame ++;
+    }
+    else
     {
             SceneManager.LoadScene("Memorize2");
     }
