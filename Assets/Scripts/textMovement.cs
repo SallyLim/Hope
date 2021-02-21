0690f2d835e9675311e998978bf092a245151866
Brennan <thetrunobody@gmail.com>
2021-02-21 12:24:39 +0700

    finish scene 1

--- Assets/Scripts/textMovement.cs
+++ Assets/Scripts/textMovement.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class textMovement : MonoBehaviour
 {
-    public GameObject textComponent;
-    public GameObject targetText;
+    public GameObject textComponent1;
+    public GameObject textComponent2;
+    public GameObject textComponent3;
+    public GameObject textComponent4;
+    public GameObject targetText1;
 
-    private float speed = 2f;
+
+    private float minSpeed = 1f;
+    private float maxSpeed = 5f;
 
     void FixedUpdate()
     {
-    textComponent.transform.position = Vector3.MoveTowards (textComponent.transform.position, targetText.transform.position, Time.deltaTime * speed);
-    if (textComponent.transform.position == targetText.transform.position)
+    textComponent1.transform.position = Vector3.MoveTowards (textComponent1.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+
+    textComponent2.transform.position = Vector3.MoveTowards (textComponent2.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+
+    textComponent3.transform.position = Vector3.MoveTowards (textComponent3.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+
+    textComponent4.transform.position = Vector3.MoveTowards (textComponent4.transform.position, targetText1.transform.position, Time.deltaTime * Random.Range(minSpeed, maxSpeed));
+
+    if (textComponent1.transform.position == targetText1.transform.position)
     {
-        GameObject.Destroy(textComponent);
+            SceneManager.LoadScene("Memorize2");
     }
-    
+
     }
 
 }
