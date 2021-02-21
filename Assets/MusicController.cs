fd9730a332b1d614da4b6fb28e91350e94cacceb
VincentSastra <vincent.sastra@gmail.com>
2021-02-21 02:35:30 -0800

    finish set up sounds

---
+++ Assets/MusicController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public MusicController ins;
+
+    public List<AudioClip> songList;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (ins == null) {
+            ins = this;
+        } 
+    }
+
+    public void setSong(int i) {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().clip = songList[i];
+        GetComponent<AudioSource>().Play();
+    }
+}
